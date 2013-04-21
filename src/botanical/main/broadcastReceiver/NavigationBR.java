b1ac0c8e1d214c2e0879805f3e0082d18b354a76
Tarik Gilani <tarik.gilani@gmail.com>
2013-04-21 21:58:02 +0100

    Nettoyage pour fonctionner sous win7 et win8 64bits

--- src/botanical/main/broadcastReceiver/NavigationBR.java
+++ src/botanical/main/broadcastReceiver/NavigationBR.java
@@ -3,8 +3,8 @@ package botanical.main.broadcastReceiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
-import android.widget.Toast;
+/*import android.util.Log;
+import android.widget.Toast;*/
 
 public class NavigationBR extends BroadcastReceiver {
 	
